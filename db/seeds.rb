43f931d421f175da3ea9470323de6a9cb5f49d00
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-10 15:12:53 +0900

    Add CR methods to Restaurant model, and C methods to Review model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+restaurant_name_endings = %w[Cafe Restaurant Bistro]
+
+5.times do
+  Restaurant.create!(
+    name: "#{Faker::Games::Pokemon.move} #{restaurant_name_endings.sample}",
+    address: Faker::Games::Pokemon.location,
+    phone_number: Faker::PhoneNumber.extension,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
